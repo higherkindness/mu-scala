6bc8f85b2b6384f6cc33e5bf7c2ecdf1383f10d5
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-09-05 17:02:32 +0200

    Moves examples to its own repository

--- scripts/gimme.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/bin/gimme
-curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme;
-chmod +x ~/bin/gimme;
--- scripts/protobuf.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-wget https://github.com/google/protobuf/archive/v3.3.1.tar.gz
-tar -xzvf v3.3.1.tar.gz
-cd protobuf-3.3.1 && ./autogen.sh && ./configure --prefix=/usr && make && sudo make install
