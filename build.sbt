9ca9f0b86dd162f94aa10a943a768bc533959d0d
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-10-18 16:11:30 +0200

    Test Coverage for some client definitions (#57)
    * Tests for FutureInstances implicit def
    
    * Disables coverage for macros and annotations code
    
    * Adds TaskMHandler unit tests

--- build.sbt
+++ build.sbt
@@ -2,6 +2,13 @@ pgpPassphrase := Some(getEnvVar("PGP_PASSPHRASE").getOrElse("").toCharArray)
 pgpPublicRing := file(s"$gpgFolder/pubring.gpg")
 pgpSecretRing := file(s"$gpgFolder/secring.gpg")
 
+lazy val root = project
+  .in(file("."))
+  .settings(name := "freestyle-rpc")
+  .settings(noPublishSettings)
+  .dependsOn(common, rpc)
+  .aggregate(common, rpc)
+
 lazy val common = project
   .in(file("common"))
   .settings(moduleName := "frees-rpc-common")
---
+++ rpc/src/test/scala/client/ImplicitTests.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017 47 Degrees, LLC. <http://www.47deg.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package freestyle.rpc
+package client
+
+import freestyle.FSHandler
+import monix.eval.Task
+
+import scala.concurrent.Future
+
+class ImplicitTests extends RpcClientTestSuite {
+
+  type FSHandlerTask2Future = FSHandler[Task, Future]
+
+  "FutureInstances.task2Future" should {
+
+    "provide an implicit evidence allowing to transform from monix.eval.Task to scala.concurrent.Future" in {
+
+      implicit val S: monix.execution.Scheduler = monix.execution.Scheduler.Implicits.global
+      import freestyle.async.implicits.futureAsyncContext
+
+      freestyle.rpc.client.implicits.task2Future shouldBe a[FSHandlerTask2Future]
+    }
+
+    "fail compiling when the monix.execution.Scheduler implicit evidence is not present" in {
+
+      shapeless.test.illTyped(
+        """freestyle.rpc.client.implicits.task2Future""",
+        ".*could not find implicit value for parameter AC.*"
+      )
+    }
+
+    "fail compiling when the freestyle.async.AsyncContext implicit evidence is not present" in {
+
+      implicit val S: monix.execution.Scheduler = monix.execution.Scheduler.Implicits.global
+
+      shapeless.test.illTyped(
+        """freestyle.rpc.client.implicits.task2Future""",
+        ".*could not find implicit value for parameter AC.*"
+      )
+    }
+
+  }
+
+}
