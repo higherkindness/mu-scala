54cb002e0f5fa59f544ba733866de629addb9254
Laurence Lavigne <laurence.lavigne@47deg.com>
2018-03-23 10:37:02 -0700

    Disabled a test that fails intermittently on Travis

--- modules/prometheus/client/src/test/scala/BaseMonitorClientInterceptorTests.scala
+++ modules/prometheus/client/src/test/scala/BaseMonitorClientInterceptorTests.scala
@@ -106,7 +106,8 @@ abstract class BaseMonitorClientInterceptorTests extends RpcBaseTestSuite {
 
     }
 
-    "work for client streaming RPC metrics" in {
+    // TODO: restore once https://github.com/frees-io/freestyle-rpc/issues/168 is fixed
+    "work for client streaming RPC metrics" ignore {
 
       def clientProgram[F[_]](implicit APP: MyRPCClient[F]): F[D] =
         APP.cs(cList, i)
