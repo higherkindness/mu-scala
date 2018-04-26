b07e94908b9ac8e0509c42f8e1b044b10f0675d8
JesusMtnez <jesus.martinez@47deg.com>
2018-04-26 08:32:51 +0200

    Use snapshot version only for required dependencies

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -23,7 +23,7 @@ object ProjectPlugin extends AutoPlugin {
       val avrohugger: String         = "1.0.0-RC4"
       val catsEffect: String         = "0.10.1"
       val circe: String              = "0.9.3"
-      val frees: String              = "0.8.1-SNAPSHOT"
+      val frees: String              = "0.8.0"
       val fs2ReactiveStreams: String = "0.5.1"
       val grpc: String               = "1.11.0"
       val log4s: String              = "1.6.1"
@@ -164,7 +164,7 @@ object ProjectPlugin extends AutoPlugin {
 
     lazy val exampleTodolistCommonSettings: Seq[Def.Setting[_]] = Seq(
       libraryDependencies ++= Seq(
-        "io.frees" %% "frees-todolist-lib" % V.frees,
+        "io.frees" %% "frees-todolist-lib" % "0.8.1-SNAPSHOT",
         %%("log4s", V.log4s),
         %("logback-classic", V.logback)
       )
