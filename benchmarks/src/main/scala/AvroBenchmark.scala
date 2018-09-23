fac3e70e8091550947fa7e2fb5d179141ac6fd99
Juan Pedro Moreno <juanpmorest@gmail.com>
2018-09-23 16:07:29 +0200

    Adds the first frees-rpc benchmarks

---
+++ benchmarks/src/main/scala/AvroBenchmark.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2017-2018 47 Degrees, LLC. <http://www.47deg.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package freestyle.rpc.benchmarks
+
+import cats.effect.IO
+import freestyle.rpc.protocol.Empty
+import java.util.concurrent.TimeUnit
+
+import freestyle.rpc.benchmarks.Utils._
+import freestyle.rpc.benchmarks.shared.models._
+import freestyle.rpc.benchmarks.shared.PersonServiceAvro
+import org.openjdk.jmh.annotations._
+
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.SECONDS)
+class AvroBenchmark {
+
+  import freestyle.rpc.benchmarks.shared.implicits._
+
+  val client: PersonServiceAvro.Client[IO] = PersonServiceAvro.client[IO](channel)
+
+  @Benchmark
+  def listPersons: PersonList = client.listPersons(Empty).unsafeRunTimed(defaultTimeOut).get
+
+  @Benchmark
+  def getPerson: Person = client.getPerson(PersonId("1")).unsafeRunTimed(defaultTimeOut).get
+
+  @Benchmark
+  def getPersonLinks: PersonLinkList =
+    client.getPersonLinks(PersonId("1")).unsafeRunTimed(defaultTimeOut).get
+
+  @Benchmark
+  def createPerson: Person =
+    client.createPerson(person).unsafeRunTimed(defaultTimeOut).get
+
+  @Benchmark
+  def programComposition: PersonAggregation = {
+
+    def clientProgram: IO[PersonAggregation] = {
+      for {
+        personList <- client.listPersons(Empty)
+        p1         <- client.getPerson(PersonId("1"))
+        p2         <- client.getPerson(PersonId("2"))
+        p3         <- client.getPerson(PersonId("3"))
+        p4         <- client.getPerson(PersonId("4"))
+        p1Links    <- client.getPersonLinks(PersonId(p1.id))
+        p3Links    <- client.getPersonLinks(PersonId(p3.id))
+        pNew       <- client.createPerson(person)
+      } yield (p1, p2, p3, p4, p1Links, p3Links, personList.add(pNew))
+    }
+
+    clientProgram.unsafeRunTimed(defaultTimeOut).get
+  }
+
+}
