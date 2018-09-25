446d998fd4d7b8f5002d7e6b67f7a2be7a223726
Juan Pedro Moreno <juanpmorest@gmail.com>
2018-09-25 12:09:28 +0200

    Addresses code review comment

--- modules/examples/todolist/runtime/src/main/scala/CommonRuntime.scala
+++ modules/examples/todolist/runtime/src/main/scala/CommonRuntime.scala
@@ -20,6 +20,6 @@ import scala.concurrent.ExecutionContext
 
 trait CommonRuntime {
 
-  implicit val EC: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+  implicit val EC: ExecutionContext = ExecutionContext.Implicits.global
 
 }
