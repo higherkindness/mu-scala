eb82629ccce5085edd6e301ec24056ba6b3fe1f9
Javier de Siloniz Sandino <jsandino@starbuck.com>
2019-02-26 09:01:51 -0800

    Added basic draft for Prometheus Metrics

--- modules/metrics/prometheus/src/main/scala/higherkindness/mu/rpc/prometheus/PrometheusMetrics.scala
+++ modules/metrics/prometheus/src/main/scala/higherkindness/mu/rpc/prometheus/PrometheusMetrics.scala
@@ -16,11 +16,73 @@
 
 package higherkindness.mu.rpc.prometheus
 
-import io.prometheus.client.CollectorRegistry
+import cats.effect.Sync
+import higherkindness.mu.rpc.internal.interceptors.GrpcMethodInfo
+import io.prometheus.client.{CollectorRegistry, Counter, Gauge, Summary}
 import higherkindness.mu.rpc.internal.metrics.MetricsOps
+import io.grpc.Status
+
+case class PrometheusMetrics(
+                              activeCalls: Gauge,
+                              messagesSent: Counter,
+                              messagesReceived: Counter,
+                              headersTime: Summary,
+                              totalTime: Summary
+                            )
 
 object PrometheusMetrics {
 
-  def apply[F[_]](cr: CollectorRegistry, prefix: String = "higherkinderness.mu"): F[MetricsOps[F]] = ???
+  def apply[F[_]](cr: CollectorRegistry, prefix: String = "higherkinderness.mu")(implicit F: Sync[F]) = F.delay {
+    new MetricsOps[F] {
+      override def increaseActiveCalls(methodInfo: GrpcMethodInfo, classifier: Option[String]): F[Unit] = ???
+
+      override def decreaseActiveCalls(methodInfo: GrpcMethodInfo, classifier: Option[String]): F[Unit] = ???
+
+      override def recordMessageSent(methodInfo: GrpcMethodInfo, classifier: Option[String]): F[Unit] = ???
+
+      override def recordMessageReceived(methodInfo: GrpcMethodInfo, classifier: Option[String]): F[Unit] = ???
+
+      override def recordHeadersTime(methodInfo: GrpcMethodInfo, elapsed: Long, classifier: Option[String]): F[Unit] = ???
+
+      override def recordTotalTime(methodInfo: GrpcMethodInfo, status: Status, elapsed: Long, classifier: Option[String]): F[Unit] = ???
+    }
+  }
+
+  def metrics(prefix: String, classifier: Option[String], registry: CollectorRegistry) = PrometheusMetrics(
+    activeCalls = Gauge
+      .build()
+      .name(s"${prefix}_${classifier}_active_calls")
+      .help("Current active calls.")
+      .labelNames("active_calls")
+      .register(registry),
+    messagesSent =
+      Counter
+        .build()
+        .name(s"${prefix}_${classifier}_messages_sent")
+        .help("Number of messages sent.")
+        .labelNames("messages_sent")
+        .register(registry),
+    messagesReceived =
+      Counter
+        .build()
+        .name(s"${prefix}_${classifier}_messages_received")
+        .help("Number of messages received.")
+        .labelNames("messages_received")
+        .register(registry),
+    headersTime =
+      Summary
+        .build()
+        .name(s"${prefix}_${classifier}_calls_header")
+        .help("Accumulative time for header calls")
+        .labelNames("time", "header")
+        .register(registry),
+    totalTime =
+      Summary
+        .build()
+        .name(s"${prefix}_${classifier}_calls_total")
+        .help("Total time for all calls")
+        .labelNames("time", "total")
+        .register(registry)
+  )
 
 }
