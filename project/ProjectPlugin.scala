6241ba465c5ff523e9ca5680fb576d2a57dd560e
rafaparadela <rafa.p@47deg.com>
2019-03-18 13:54:44 -0700

    adapts latest changes in the code generation in skeuomorph

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -50,7 +50,7 @@ object ProjectPlugin extends AutoPlugin {
       val scalacheckToolbox: String  = "0.2.5"
       val scalamockScalatest: String = "3.6.0"
       val scalatest: String          = "3.0.6"
-      val skeuomorph: String         = "0.0.5"
+      val skeuomorph: String         = "0.0.6-SNAPSHOT"
       val slf4j: String              = "1.7.26"
       val dropwizard: String         = "4.0.5"
     }
