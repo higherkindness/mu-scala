5d4e49ad472b0f076dafd8092885278b58df5086
Ben Fradet <benjamin.fradet@gmail.com>
2019-09-09 15:19:28 +0200

    Support creating partitions

---
+++ modules/kafka/src/main/scala/higherkindness/mu/rpc/kafka/handler/KafkaManagementServiceImpl.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017-2019 47 Degrees, LLC. <http://www.47deg.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package higherkindness.mu.rpc.kafka.handler
+
+import cats.effect.{Concurrent, ContextShift, Resource}
+import cats.implicits._
+import fs2.kafka._
+import higherkindness.mu.rpc.kafka.KafkaManagementService._
+import higherkindness.mu.rpc.protocol.Empty
+import org.apache.kafka.clients.admin.NewPartitions
+import org.apache.kafka.clients.admin.NewTopic
+
+object KafkaManagement {
+  def buildInstance[F[_]: ContextShift: Concurrent](): Resource[F, KafkaManagement[F]] =
+    adminClientResource[F](AdminClientSettings.apply[F])
+      .map(c => new KafkaManagementImpl(c))
+
+  class KafkaManagementImpl[F[_]: ContextShift: Concurrent](
+      adminClient: KafkaAdminClient[F]
+  ) extends KafkaManagement[F] {
+    override def createPartitions(cpr: CreatePartitionsRequest): F[Unit] =
+      adminClient.createPartitions(cpr.ps.mapValues(NewPartitions.increaseTo))
+  }
+}
