f591295e7a0f09877b3109b94508c5a6a692c833
Scala Steward <me@scala-steward.org>
2019-09-21 02:04:50 +0200

    Update netty-tcnative-boringssl-static to 2.0.25.Final

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -42,7 +42,7 @@ object ProjectPlugin extends AutoPlugin {
       val logback: String           = "1.2.3"
       val monix: String             = "3.0.0-RC2"
       val monocle: String           = "1.5.1-cats"
-      val nettySSL: String          = "2.0.20.Final"
+      val nettySSL: String          = "2.0.25.Final"
       val paradise: String          = "2.1.1"
       val pbdirect: String          = "0.2.1"
       val prometheus: String        = "0.6.0"
