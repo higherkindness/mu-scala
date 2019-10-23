750552248e3e2b3832a073f870934e09641f78a3
Scala Steward <me@scala-steward.org>
2019-10-23 20:54:32 +0200

    Update simpleclient to 0.8.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -47,7 +47,7 @@ object ProjectPlugin extends AutoPlugin {
       val nettySSL: String           = "2.0.25.Final"
       val paradise: String           = "2.1.1"
       val pbdirect: String           = "0.3.1"
-      val prometheus: String         = "0.7.0"
+      val prometheus: String         = "0.8.0"
       val pureconfig: String         = "0.12.1"
       val reactiveStreams: String    = "1.0.3"
       val scala: String              = "2.12.10"
