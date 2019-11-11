baea9f9238ef62707c6dc5cc18cf26dd3ed81b94
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-11-11 23:31:29 +0100

    Update java-runtime to 0.5.5 (#700)

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -34,7 +34,7 @@ object ProjectPlugin extends AutoPlugin {
       val embeddedKafka: String      = "2.3.1"
       val frees: String              = "0.8.2"
       val fs2: String                = "2.1.0"
-      val fs2Grpc: String            = "0.5.4"
+      val fs2Grpc: String            = "0.5.5"
       val fs2Kafka: String           = "0.20.2"
       val grpc: String               = "1.24.2"
       val jodaTime: String           = "2.10.5"
