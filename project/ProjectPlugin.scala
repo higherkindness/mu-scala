0dab66791fef6caf7123df613213ba351475049a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-12-23 09:18:34 +0100

    Update pureconfig to 0.12.2 (#729)

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -51,7 +51,7 @@ object ProjectPlugin extends AutoPlugin {
       val paradise: String           = "2.1.1"
       val pbdirect: String           = "0.3.1"
       val prometheus: String         = "0.8.0"
-      val pureconfig: String         = "0.12.1"
+      val pureconfig: String         = "0.12.2"
       val reactiveStreams: String    = "1.0.3"
       val scala: String              = "2.12.10"
       val scopt: String              = "3.7.1"
