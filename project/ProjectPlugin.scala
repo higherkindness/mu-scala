8fd8e44e9d05d8b99ff539d60617f77a1f184f67
Chris Birchall <chris.birchall@47deg.com>
2020-01-16 18:37:21 +0000

    Upgrade sbt-org-policies
    Sonatype bundle releases should work with this version.

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -283,7 +283,7 @@ object ProjectPlugin extends AutoPlugin {
       // Custom release process for the plugin:
       releaseProcess := Seq[ReleaseStep](
         releaseStepCommandAndRemaining("^ publishSigned"),
-        ReleaseStep(action = "sonatypeReleaseAll" :: _)
+        releaseStepCommand("sonatypeBundleRelease")
       )
     )
 
@@ -375,7 +375,7 @@ object ProjectPlugin extends AutoPlugin {
         orgTagRelease,
         orgUpdateChangeLog,
         releaseStepCommandAndRemaining("publishSigned"),
-        releaseStepCommand("sonatypeReleaseAll"),
+        releaseStepCommand("sonatypeBundleRelease"),
         setNextVersion,
         orgCommitNextVersion,
         orgPostRelease
