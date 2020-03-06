6558d9a74a6c99530fd8f3e087952f8ea8cfb7c9
Scala Steward <me@scala-steward.org>
2020-03-06 01:24:38 +0100

    Update monocle-core to 2.0.4

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -44,7 +44,7 @@ object ProjectPlugin extends AutoPlugin {
       val log4s: String               = "1.8.2"
       val logback: String             = "1.2.3"
       val monix: String               = "3.1.0"
-      val monocle: String             = "2.0.2"
+      val monocle: String             = "2.0.4"
       val lastRelease                 = "0.20.1"
       val nettySSL: String            = "2.0.25.Final"
       val paradise: String            = "2.1.1"
