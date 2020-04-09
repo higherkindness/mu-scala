f10a9f960db2852be1acb01df48f867345b6de00
Scala Steward <me@scala-steward.org>
2020-04-09 22:20:14 +0200

    Update metrics-core to 4.1.6

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -29,7 +29,7 @@ object ProjectPlugin extends AutoPlugin {
       val catsEffect: String          = "2.1.2"
       val circe: String               = "0.13.0"
       val dockerItScala               = "0.9.9"
-      val dropwizard: String          = "4.1.5"
+      val dropwizard: String          = "4.1.6"
       val embeddedKafka: String       = "2.4.1"
       val enumeratum: String          = "1.5.15"
       val frees: String               = "0.8.2"
