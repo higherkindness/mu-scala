cdadb5fc74b6001d0b69ea3926fdfc0994f9089f
Scala Steward <me@scala-steward.org>
2020-04-23 22:41:06 +0200

    Update embedded-kafka to 2.5.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -22,7 +22,7 @@ object ProjectPlugin extends AutoPlugin {
       val circe: String                 = "0.13.0"
       val dockerItScala                 = "0.9.9"
       val dropwizard: String            = "4.1.6"
-      val embeddedKafka: String         = "2.4.1.1"
+      val embeddedKafka: String         = "2.5.0"
       val enumeratum: String            = "1.5.15"
       val fs2: String                   = "2.3.0"
       val fs2Grpc: String               = "0.7.0"
