d1334a78d5ee6c52834ed0820da4f7f60012cf44
Scala Steward <me@scala-steward.org>
2020-04-26 14:18:08 +0200

    Update monix to 3.2.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -35,7 +35,7 @@ object ProjectPlugin extends AutoPlugin {
       val log4cats: String              = "1.0.1"
       val log4s: String                 = "1.8.2"
       val logback: String               = "1.2.3"
-      val monix: String                 = "3.1.0"
+      val monix: String                 = "3.2.0"
       val natchez: String               = "0.0.11"
       val nettySSL: String              = "2.0.28.Final"
       val paradise: String              = "2.1.1"
