405a0db60e43cfae9a50962fa16e96a46fc25213
Scala Steward <me@scala-steward.org>
2020-05-10 14:37:54 +0200

    Update scalatest to 3.1.2

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -49,7 +49,7 @@ object ProjectPlugin extends AutoPlugin {
       val scalacheck: String            = "1.14.3"
       val scalacheckToolbox: String     = "0.3.5"
       val scalamock: String             = "4.4.0"
-      val scalatest: String             = "3.1.1"
+      val scalatest: String             = "3.1.2"
       val scalatestplusScheck: String   = "3.1.1.1"
       val slf4j: String                 = "1.7.30"
     }
