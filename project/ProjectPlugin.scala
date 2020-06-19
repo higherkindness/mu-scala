16f5a1f8846fa7fe61c37811aaafd76fc794e8d0
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-06-19 09:27:34 +0200

    Update scalacheck-1-14 to 3.2.0.0 (#947)

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -45,7 +45,7 @@ object ProjectPlugin extends AutoPlugin {
       val scalacheckToolbox: String     = "0.3.5"
       val scalamock: String             = "4.4.0"
       val scalatest: String             = "3.1.2"
-      val scalatestplusScheck: String   = "3.1.2.0"
+      val scalatestplusScheck: String   = "3.2.0.0"
       val slf4j: String                 = "1.7.30"
     }
 
