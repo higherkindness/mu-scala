365f3d8ad0687faf703cfb18bec8d05d109a1383
Fede Fernández <720923+fedefernandez@users.noreply.github.com>
2020-11-27 17:17:09 +0100

    Bumps http4s (#966)
    * Bumps http4s
    
    * Fix test
    
    * Decode list from a request stream
    
    * Scalafmt
    
    * Bumps http4s
    
    Co-authored-by: Juan Pedro Moreno <4879373+juanpedromoreno@users.noreply.github.com>
    
    * Update modules/http/src/main/scala/higherkindness/mu/http/implicits.scala
    
    Co-authored-by: Roberto Serrano <r.semarcos@gmail.com>
    
    * Fix compilation error
    
    Co-authored-by: Juan Pedro Moreno <4879373+juanpedromoreno@users.noreply.github.com>
    Co-authored-by: Roberto Serrano <r.semarcos@gmail.com>

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -26,7 +26,7 @@ object ProjectPlugin extends AutoPlugin {
       val fs2Grpc: String               = "0.8.0"
       val fs2Kafka: String              = "1.0.0"
       val grpc: String                  = "1.33.1"
-      val http4s: String                = "0.21.0-M6"
+      val http4s: String                = "0.21.7"
       val kindProjector: String         = "0.11.1"
       val log4cats: String              = "1.1.1"
       val log4s: String                 = "1.9.0"
