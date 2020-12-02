7a1262368055c9f3c238457401d493fd51abac45
47erbot <developer@47deg.com>
2020-12-02 05:16:38 +0000

    Update kind-projector to 0.11.2

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -27,7 +27,7 @@ object ProjectPlugin extends AutoPlugin {
       val fs2Kafka: String              = "1.0.0"
       val grpc: String                  = "1.33.1"
       val http4s: String                = "0.21.13"
-      val kindProjector: String         = "0.11.1"
+      val kindProjector: String         = "0.11.2"
       val log4cats: String              = "1.1.1"
       val log4s: String                 = "1.9.0"
       val logback: String               = "1.2.3"
