1d169e1f92e9d788f82fb65d2ffcf9ab05df768b
47erbot <developer@47deg.com>
2021-03-29 05:35:24 +0000

    Update fs2-kafka to 1.4.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -24,7 +24,7 @@ object ProjectPlugin extends AutoPlugin {
       val enumeratum: String            = "1.6.1"
       val fs2: String                   = "2.5.3"
       val fs2Grpc: String               = "0.9.0"
-      val fs2Kafka: String              = "1.1.0"
+      val fs2Kafka: String              = "1.4.1"
       val grpc: String                  = "1.36.1"
       val http4s: String                = "0.21.20"
       val kindProjector: String         = "0.11.3"
