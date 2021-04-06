1c208c511828dbdecb8726667a8e6f06f95544c2
Juan Pedro Moreno <juanpmorest@gmail.com>
2021-04-06 21:36:22 +0200

    https://github.com/grpc/grpc-java/blob/v1.36.1/build.gradle\#L179

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -34,7 +34,7 @@ object ProjectPlugin extends AutoPlugin {
       val scalalogging: String          = "3.9.3" // used in tests
       val monix: String                 = "3.3.0"
       val natchez: String               = "0.0.22"
-      val nettySSL: String              = "2.0.38.Final"
+      val nettySSL: String              = "2.0.34.Final"
       val paradise: String              = "2.1.1"
       val pbdirect: String              = "0.6.1"
       val prometheus: String            = "0.10.0"
