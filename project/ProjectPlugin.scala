37bf9eff1d05b19affe8075c6c0a64e3e15e8cf8
47erbot <developer@47deg.com>
2021-04-06 10:59:33 +0200

    Update simpleclient to 0.10.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -37,7 +37,7 @@ object ProjectPlugin extends AutoPlugin {
       val nettySSL: String              = "2.0.38.Final"
       val paradise: String              = "2.1.1"
       val pbdirect: String              = "0.6.1"
-      val prometheus: String            = "0.9.0"
+      val prometheus: String            = "0.10.0"
       val pureconfig: String            = "0.14.1"
       val reactiveStreams: String       = "1.0.3"
       val scalaCollectionCompat: String = "2.4.3"
