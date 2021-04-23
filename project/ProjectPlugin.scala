c0c0d71429c6a31e039be241c003aa339aafdc3f
47erbot <developer@47deg.com>
2021-04-23 05:39:37 +0000

    Update embedded-kafka to 2.8.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -20,7 +20,7 @@ object ProjectPlugin extends AutoPlugin {
       val circe: String                 = "0.13.0"
       val dockerItScala                 = "0.9.9"
       val dropwizard: String            = "4.1.19"
-      val embeddedKafka: String         = "2.7.0"
+      val embeddedKafka: String         = "2.8.0"
       val enumeratum: String            = "1.6.1"
       val fs2: String                   = "2.5.4"
       val fs2Grpc: String               = "0.10.2"
