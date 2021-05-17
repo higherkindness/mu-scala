f98c5086e94bdc2b1437dcd8f882a978695c5eb2
47erbot <developer@47deg.com>
2021-05-17 05:51:40 +0000

    Update java-runtime to 0.11.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -23,7 +23,7 @@ object ProjectPlugin extends AutoPlugin {
       val embeddedKafka: String         = "2.7.0"
       val enumeratum: String            = "1.6.1"
       val fs2: String                   = "2.5.6"
-      val fs2Grpc: String               = "0.10.3"
+      val fs2Grpc: String               = "0.11.0"
       val fs2Kafka: String              = "1.5.0"
       val grpc: String                  = "1.36.1"
       val http4s: String                = "0.21.22"
