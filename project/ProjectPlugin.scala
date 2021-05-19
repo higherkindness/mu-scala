1b25e0212919bd38ab9db329a8bba573c0508e3f
47erbot <developer@47deg.com>
2021-05-19 05:31:43 +0000

    Update log4cats-core, log4cats-slf4j to 1.3.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -28,7 +28,7 @@ object ProjectPlugin extends AutoPlugin {
       val grpc: String                  = "1.36.1"
       val http4s: String                = "0.21.23"
       val kindProjector: String         = "0.13.0"
-      val log4cats: String              = "1.3.0"
+      val log4cats: String              = "1.3.1"
       val log4s: String                 = "1.9.0"
       val logback: String               = "1.2.3"
       val scalalogging: String          = "3.9.3" // used in tests
