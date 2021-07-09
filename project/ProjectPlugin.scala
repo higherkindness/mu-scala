0b4bebfbed633ad298a07736f8041260e1da6cf8
47erbot <developer@47deg.com>
2021-07-09 07:31:23 +0200

    Update scala-collection-compat to 2.5.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -38,7 +38,7 @@ object ProjectPlugin extends AutoPlugin {
       val prometheus: String            = "0.10.0"
       val pureconfig: String            = "0.16.0"
       val reactiveStreams: String       = "1.0.3"
-      val scalaCollectionCompat: String = "2.4.4"
+      val scalaCollectionCompat: String = "2.5.0"
       val scalacheckToolbox: String     = "0.6.0"
       val scalamock: String             = "5.1.0"
       val scalatest: String             = "3.2.9"
