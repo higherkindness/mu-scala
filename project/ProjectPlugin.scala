3059999766ee8da6b9c32b91bd7e01492a53d4ad
Juan Pedro Moreno <4879373+juanpedromoreno@users.noreply.github.com>
2021-09-27 13:15:09 +0200

    Updates pbdirect 0.7.0 (#1318)

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -32,7 +32,7 @@ object ProjectPlugin extends AutoPlugin {
       val natchez: String               = "0.1.4"
       val nettySSL: String              = "2.0.34.Final"
       val paradise: String              = "2.1.1"
-      val pbdirect: String              = "0.6.1"
+      val pbdirect: String              = "0.7.0"
       val prometheus: String            = "0.10.0"
       val pureconfig: String            = "0.16.0"
       val scalaCollectionCompat: String = "2.5.0"
