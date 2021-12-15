c37c397b998baa4667d0cf5a12db0dbf90030a52
47erbot <developer@47deg.com>
2021-12-15 05:25:52 +0000

    Update grpc-all, grpc-netty, grpc-okhttp, ... to 1.43.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -22,7 +22,7 @@ object ProjectPlugin extends AutoPlugin {
       val enumeratum: String            = "1.7.0"
       val fs2: String                   = "3.2.3"
       val fs2Grpc: String               = "2.3.0"
-      val grpc: String                  = "1.42.1"
+      val grpc: String                  = "1.43.0"
       val kindProjector: String         = "0.13.2"
       val log4cats: String              = "2.1.1"
       val log4s: String                 = "1.10.0"
