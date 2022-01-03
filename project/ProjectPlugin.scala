7f56f5bede16461380ce5741d6a876cde310fdbb
47erbot <developer@47deg.com>
2022-01-03 09:09:38 +0100

    Update fs2-grpc-runtime to 2.3.2

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -21,7 +21,7 @@ object ProjectPlugin extends AutoPlugin {
       val dropwizard: String            = "4.2.7"
       val enumeratum: String            = "1.7.0"
       val fs2: String                   = "3.2.4"
-      val fs2Grpc: String               = "2.3.1"
+      val fs2Grpc: String               = "2.3.2"
       val grpc: String                  = "1.43.1"
       val kindProjector: String         = "0.13.2"
       val log4cats: String              = "2.1.1"
