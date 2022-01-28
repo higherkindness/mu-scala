a8fc2c44f4de564b4199e6656bfbdb87ef2d09c4
47erbot <developer@47deg.com>
2022-01-28 06:32:15 +0100

    Update scalatest to 3.2.11

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -38,7 +38,7 @@ object ProjectPlugin extends AutoPlugin {
       val scalaCollectionCompat: String = "2.6.0"
       val scalacheckToolbox: String     = "0.6.0"
       val scalamock: String             = "5.1.0"
-      val scalatest: String             = "3.2.10"
+      val scalatest: String             = "3.2.11"
       val scalatestplusScheck: String   = "3.2.2.0"
       val slf4j: String                 = "1.7.35"
     }
