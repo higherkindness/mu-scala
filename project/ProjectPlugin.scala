4a2c31a531caa566dd5b1f031ca3f50d2349f458
47erbot <developer@47deg.com>
2022-02-09 06:35:51 +0100

    Update jcl-over-slf4j, jul-to-slf4j, ... to 1.7.36

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -40,7 +40,7 @@ object ProjectPlugin extends AutoPlugin {
       val scalamock: String             = "5.1.0"
       val scalatest: String             = "3.2.11"
       val scalatestplusScheck: String   = "3.2.2.0"
-      val slf4j: String                 = "1.7.35"
+      val slf4j: String                 = "1.7.36"
     }
 
     lazy val rpcServiceSettings: Seq[Def.Setting[_]] = Seq(
