72867944183a737863a9f83af141976712033be9
47erbot <developer@47deg.com>
2022-04-01 07:35:26 +0200

    Update fs2-core to 3.2.7

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -20,7 +20,7 @@ object ProjectPlugin extends AutoPlugin {
       val dockerItScala                 = "0.9.9"
       val dropwizard: String            = "4.2.9"
       val enumeratum: String            = "1.7.0"
-      val fs2: String                   = "3.2.5"
+      val fs2: String                   = "3.2.7"
       val fs2Grpc: String               = "2.4.7"
       val grpc: String                  = "1.45.0"
       val kindProjector: String         = "0.13.2"
