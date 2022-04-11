2b17257d2e6e6642b12e0e04f431526c4d40743b
Chris Birchall <chris.birchall@47deg.com>
2022-04-11 15:29:51 +0100

    Add a TaggedDecimal type to replace Shapeless tagging
    `TaggedDecimal[P <: Int, S <: Int]` is a BigDecimal tagged with the
    precision and scale extracted from its corresponding `decimal` logical
    type in the Avro schema.
    
    Implemented as an opaque type, with a smart constructor that performs a
    couple of sanity checks on the precision and scale of the supplied
    BigDecimal.

---
+++ modules/service/src/main/scala-3/higherkindness/mu/rpc/avro/Decimals.scala
@@ -0,0 +1,35 @@
+package higherkindness.mu.rpc.avro
+
+import org.apache.avro.Schema
+import com.sksamuel.avro4s._
+import scala.compiletime.ops.int._
+
+object Decimals:
+
+  opaque type TaggedDecimal[P <: Int, S <: Int] = BigDecimal
+
+  extension [P <: Int, S <: Int] (td: TaggedDecimal[P, S])
+    def value: BigDecimal = td
+
+  object TaggedDecimal:
+
+    def apply[P <: Int, S <: Int](bd: BigDecimal)(using p: ValueOf[P], s: ValueOf[S], scaleNotGreaterThanPrecision: S <= P =:= true): Either[String, TaggedDecimal[P, S]] =
+      if (bd.precision > p.value) {
+        Left(s"Precision is too high. Maximum allowed precision is ${p.value}, but this BigDecimal has precision ${bd.precision}.")
+      } else if ((s.value - bd.scale) > (p.value - bd.precision)) {
+        Left(s"Scale (${bd.scale}) is too low. Calling setScale(${s.value}) would cause precision to become ${bd.precision + (s.value - bd.scale)}, which exceeds the maximum precision (${p.value}).")
+      } else {
+        Right(bd)
+      }
+
+    given [P <: Int, S <: Int](using p: ValueOf[P], s: ValueOf[S]): SchemaFor[TaggedDecimal[P, S]] with
+      def schema: Schema =
+        val sp = ScalePrecision(s.value, p.value)
+        new BigDecimalSchemaFor(sp).schema
+
+    given [P <: Int, S <: Int]: Encoder[TaggedDecimal[P, S]] =
+      Encoder.given_Encoder_BigDecimal
+
+    given [P <: Int, S <: Int]: Decoder[TaggedDecimal[P, S]] =
+      Decoder.given_Decoder_BigDecimal
+
