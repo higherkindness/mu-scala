dd31b691dfbf060f7c79d6d9655250cb0d4a609a
Chris Birchall <chris.birchall@47deg.com>
2022-04-27 16:22:22 +0100

    Fix scalafmt config for Scala 3 files

--- modules/service/src/main/scala-3/higherkindness/mu/rpc/avro/AvroUnions.scala
+++ modules/service/src/main/scala-3/higherkindness/mu/rpc/avro/AvroUnions.scala
@@ -11,7 +11,9 @@ final case class AvroUnion4[T1, T2, T3, T4](value: T1 | T2 | T3 | T4)
 final case class AvroUnion5[T1, T2, T3, T4, T5](value: T1 | T2 | T3 | T4 | T5)
 final case class AvroUnion6[T1, T2, T3, T4, T5, T6](value: T1 | T2 | T3 | T4 | T5 | T6)
 final case class AvroUnion7[T1, T2, T3, T4, T5, T6, T7](value: T1 | T2 | T3 | T4 | T5 | T6 | T7)
-final case class AvroUnion8[T1, T2, T3, T4, T5, T6, T7, T8](value: T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8)
+final case class AvroUnion8[T1, T2, T3, T4, T5, T6, T7, T8](
+    value: T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8
+)
 
 object Utils {
 
@@ -20,7 +22,10 @@ object Utils {
 
   def decodingException(value: Any, schemas: Schema*): Avro4sDecodingException = {
     val schemaNames = schemas.map(_.getFullName).mkString("[", ", ", "]")
-    new Avro4sDecodingException(s"Could not decode union value [$value] (${value.getClass}) using any of these schemas: $schemaNames", value)
+    new Avro4sDecodingException(
+      s"Could not decode union value [$value] (${value.getClass}) using any of these schemas: $schemaNames",
+      value
+    )
   }
 
 }
@@ -30,8 +35,8 @@ import Utils._
 object AvroUnion2 {
 
   given [
-    T1: SchemaFor,
-    T2: SchemaFor
+      T1: SchemaFor,
+      T2: SchemaFor
   ]: SchemaFor[AvroUnion2[T1, T2]] with
     def schema: Schema =
       SchemaBuilder.unionOf
@@ -41,8 +46,8 @@ object AvroUnion2 {
         .endUnion
 
   given [
-    T1: Typeable: SchemaFor: Encoder,
-    T2: Typeable: SchemaFor: Encoder
+      T1: Typeable: SchemaFor: Encoder,
+      T2: Typeable: SchemaFor: Encoder
   ]: Encoder[AvroUnion2[T1, T2]] with
     def encode(schema: Schema): AvroUnion2[T1, T2] => Any = { union =>
       union.value match {
@@ -52,8 +57,8 @@ object AvroUnion2 {
     }
 
   given [
-    T1: SchemaFor: Decoder: TypeGuardedDecoding,
-    T2: SchemaFor: Decoder: TypeGuardedDecoding
+      T1: SchemaFor: Decoder: TypeGuardedDecoding,
+      T2: SchemaFor: Decoder: TypeGuardedDecoding
   ]: Decoder[AvroUnion2[T1, T2]] with
     def decode(schema: Schema): Any => AvroUnion2[T1, T2] = {
       val schema1 = AvroSchema[T1]
@@ -77,9 +82,9 @@ object AvroUnion2 {
 object AvroUnion3 {
 
   given [
-    T1: SchemaFor,
-    T2: SchemaFor,
-    T3: SchemaFor
+      T1: SchemaFor,
+      T2: SchemaFor,
+      T3: SchemaFor
   ]: SchemaFor[AvroUnion3[T1, T2, T3]] with
     def schema: Schema =
       SchemaBuilder.unionOf
@@ -91,9 +96,9 @@ object AvroUnion3 {
         .endUnion
 
   given [
-    T1: Typeable: SchemaFor: Encoder,
-    T2: Typeable: SchemaFor: Encoder,
-    T3: Typeable: SchemaFor: Encoder
+      T1: Typeable: SchemaFor: Encoder,
+      T2: Typeable: SchemaFor: Encoder,
+      T3: Typeable: SchemaFor: Encoder
   ]: Encoder[AvroUnion3[T1, T2, T3]] with
     def encode(schema: Schema): AvroUnion3[T1, T2, T3] => Any = { union =>
       union.value match {
@@ -104,9 +109,9 @@ object AvroUnion3 {
     }
 
   given [
-    T1: SchemaFor: Decoder: TypeGuardedDecoding,
-    T2: SchemaFor: Decoder: TypeGuardedDecoding,
-    T3: SchemaFor: Decoder: TypeGuardedDecoding
+      T1: SchemaFor: Decoder: TypeGuardedDecoding,
+      T2: SchemaFor: Decoder: TypeGuardedDecoding,
+      T3: SchemaFor: Decoder: TypeGuardedDecoding
   ]: Decoder[AvroUnion3[T1, T2, T3]] with
     def decode(schema: Schema): Any => AvroUnion3[T1, T2, T3] = {
       val schema1 = AvroSchema[T1]
@@ -134,10 +139,10 @@ object AvroUnion3 {
 object AvroUnion4 {
 
   given [
-    T1: SchemaFor,
-    T2: SchemaFor,
-    T3: SchemaFor,
-    T4: SchemaFor
+      T1: SchemaFor,
+      T2: SchemaFor,
+      T3: SchemaFor,
+      T4: SchemaFor
   ]: SchemaFor[AvroUnion4[T1, T2, T3, T4]] with
     def schema: Schema =
       SchemaBuilder.unionOf
@@ -151,10 +156,10 @@ object AvroUnion4 {
         .endUnion
 
   given [
-    T1: Typeable: SchemaFor: Encoder,
-    T2: Typeable: SchemaFor: Encoder,
-    T3: Typeable: SchemaFor: Encoder,
-    T4: Typeable: SchemaFor: Encoder
+      T1: Typeable: SchemaFor: Encoder,
+      T2: Typeable: SchemaFor: Encoder,
+      T3: Typeable: SchemaFor: Encoder,
+      T4: Typeable: SchemaFor: Encoder
   ]: Encoder[AvroUnion4[T1, T2, T3, T4]] with
     def encode(schema: Schema): AvroUnion4[T1, T2, T3, T4] => Any = { union =>
       union.value match {
@@ -166,10 +171,10 @@ object AvroUnion4 {
     }
 
   given [
-    T1: SchemaFor: Decoder: TypeGuardedDecoding,
-    T2: SchemaFor: Decoder: TypeGuardedDecoding,
-    T3: SchemaFor: Decoder: TypeGuardedDecoding,
-    T4: SchemaFor: Decoder: TypeGuardedDecoding
+      T1: SchemaFor: Decoder: TypeGuardedDecoding,
+      T2: SchemaFor: Decoder: TypeGuardedDecoding,
+      T3: SchemaFor: Decoder: TypeGuardedDecoding,
+      T4: SchemaFor: Decoder: TypeGuardedDecoding
   ]: Decoder[AvroUnion4[T1, T2, T3, T4]] with
     def decode(schema: Schema): Any => AvroUnion4[T1, T2, T3, T4] = {
       val schema1 = AvroSchema[T1]
@@ -201,11 +206,11 @@ object AvroUnion4 {
 object AvroUnion5 {
 
   given [
-    T1: SchemaFor,
-    T2: SchemaFor,
-    T3: SchemaFor,
-    T4: SchemaFor,
-    T5: SchemaFor
+      T1: SchemaFor,
+      T2: SchemaFor,
+      T3: SchemaFor,
+      T4: SchemaFor,
+      T5: SchemaFor
   ]: SchemaFor[AvroUnion5[T1, T2, T3, T4, T5]] with
     def schema: Schema =
       SchemaBuilder.unionOf
@@ -221,11 +226,11 @@ object AvroUnion5 {
         .endUnion
 
   given [
-    T1: Typeable: SchemaFor: Encoder,
-    T2: Typeable: SchemaFor: Encoder,
-    T3: Typeable: SchemaFor: Encoder,
-    T4: Typeable: SchemaFor: Encoder,
-    T5: Typeable: SchemaFor: Encoder
+      T1: Typeable: SchemaFor: Encoder,
+      T2: Typeable: SchemaFor: Encoder,
+      T3: Typeable: SchemaFor: Encoder,
+      T4: Typeable: SchemaFor: Encoder,
+      T5: Typeable: SchemaFor: Encoder
   ]: Encoder[AvroUnion5[T1, T2, T3, T4, T5]] with
     def encode(schema: Schema): AvroUnion5[T1, T2, T3, T4, T5] => Any = { union =>
       union.value match {
@@ -238,11 +243,11 @@ object AvroUnion5 {
     }
 
   given [
-    T1: SchemaFor: Decoder: TypeGuardedDecoding,
-    T2: SchemaFor: Decoder: TypeGuardedDecoding,
-    T3: SchemaFor: Decoder: TypeGuardedDecoding,
-    T4: SchemaFor: Decoder: TypeGuardedDecoding,
-    T5: SchemaFor: Decoder: TypeGuardedDecoding
+      T1: SchemaFor: Decoder: TypeGuardedDecoding,
+      T2: SchemaFor: Decoder: TypeGuardedDecoding,
+      T3: SchemaFor: Decoder: TypeGuardedDecoding,
+      T4: SchemaFor: Decoder: TypeGuardedDecoding,
+      T5: SchemaFor: Decoder: TypeGuardedDecoding
   ]: Decoder[AvroUnion5[T1, T2, T3, T4, T5]] with
     def decode(schema: Schema): Any => AvroUnion5[T1, T2, T3, T4, T5] = {
       val schema1 = AvroSchema[T1]
@@ -278,12 +283,12 @@ object AvroUnion5 {
 object AvroUnion6 {
 
   given [
-    T1: SchemaFor,
-    T2: SchemaFor,
-    T3: SchemaFor,
-    T4: SchemaFor,
-    T5: SchemaFor,
-    T6: SchemaFor
+      T1: SchemaFor,
+      T2: SchemaFor,
+      T3: SchemaFor,
+      T4: SchemaFor,
+      T5: SchemaFor,
+      T6: SchemaFor
   ]: SchemaFor[AvroUnion6[T1, T2, T3, T4, T5, T6]] with
     def schema: Schema =
       SchemaBuilder.unionOf
@@ -301,12 +306,12 @@ object AvroUnion6 {
         .endUnion
 
   given [
-    T1: Typeable: SchemaFor: Encoder,
-    T2: Typeable: SchemaFor: Encoder,
-    T3: Typeable: SchemaFor: Encoder,
-    T4: Typeable: SchemaFor: Encoder,
-    T5: Typeable: SchemaFor: Encoder,
-    T6: Typeable: SchemaFor: Encoder
+      T1: Typeable: SchemaFor: Encoder,
+      T2: Typeable: SchemaFor: Encoder,
+      T3: Typeable: SchemaFor: Encoder,
+      T4: Typeable: SchemaFor: Encoder,
+      T5: Typeable: SchemaFor: Encoder,
+      T6: Typeable: SchemaFor: Encoder
   ]: Encoder[AvroUnion6[T1, T2, T3, T4, T5, T6]] with
     def encode(schema: Schema): AvroUnion6[T1, T2, T3, T4, T5, T6] => Any = { union =>
       union.value match {
@@ -320,12 +325,12 @@ object AvroUnion6 {
     }
 
   given [
-    T1: SchemaFor: Decoder: TypeGuardedDecoding,
-    T2: SchemaFor: Decoder: TypeGuardedDecoding,
-    T3: SchemaFor: Decoder: TypeGuardedDecoding,
-    T4: SchemaFor: Decoder: TypeGuardedDecoding,
-    T5: SchemaFor: Decoder: TypeGuardedDecoding,
-    T6: SchemaFor: Decoder: TypeGuardedDecoding
+      T1: SchemaFor: Decoder: TypeGuardedDecoding,
+      T2: SchemaFor: Decoder: TypeGuardedDecoding,
+      T3: SchemaFor: Decoder: TypeGuardedDecoding,
+      T4: SchemaFor: Decoder: TypeGuardedDecoding,
+      T5: SchemaFor: Decoder: TypeGuardedDecoding,
+      T6: SchemaFor: Decoder: TypeGuardedDecoding
   ]: Decoder[AvroUnion6[T1, T2, T3, T4, T5, T6]] with
     def decode(schema: Schema): Any => AvroUnion6[T1, T2, T3, T4, T5, T6] = {
       val schema1 = AvroSchema[T1]
@@ -365,13 +370,13 @@ object AvroUnion6 {
 object AvroUnion7 {
 
   given [
-    T1: SchemaFor,
-    T2: SchemaFor,
-    T3: SchemaFor,
-    T4: SchemaFor,
-    T5: SchemaFor,
-    T6: SchemaFor,
-    T7: SchemaFor
+      T1: SchemaFor,
+      T2: SchemaFor,
+      T3: SchemaFor,
+      T4: SchemaFor,
+      T5: SchemaFor,
+      T6: SchemaFor,
+      T7: SchemaFor
   ]: SchemaFor[AvroUnion7[T1, T2, T3, T4, T5, T6, T7]] with
     def schema: Schema =
       SchemaBuilder.unionOf
@@ -391,13 +396,13 @@ object AvroUnion7 {
         .endUnion
 
   given [
-    T1: Typeable: SchemaFor: Encoder,
-    T2: Typeable: SchemaFor: Encoder,
-    T3: Typeable: SchemaFor: Encoder,
-    T4: Typeable: SchemaFor: Encoder,
-    T5: Typeable: SchemaFor: Encoder,
-    T6: Typeable: SchemaFor: Encoder,
-    T7: Typeable: SchemaFor: Encoder
+      T1: Typeable: SchemaFor: Encoder,
+      T2: Typeable: SchemaFor: Encoder,
+      T3: Typeable: SchemaFor: Encoder,
+      T4: Typeable: SchemaFor: Encoder,
+      T5: Typeable: SchemaFor: Encoder,
+      T6: Typeable: SchemaFor: Encoder,
+      T7: Typeable: SchemaFor: Encoder
   ]: Encoder[AvroUnion7[T1, T2, T3, T4, T5, T6, T7]] with
     def encode(schema: Schema): AvroUnion7[T1, T2, T3, T4, T5, T6, T7] => Any = { union =>
       union.value match {
@@ -412,13 +417,13 @@ object AvroUnion7 {
     }
 
   given [
-    T1: SchemaFor: Decoder: TypeGuardedDecoding,
-    T2: SchemaFor: Decoder: TypeGuardedDecoding,
-    T3: SchemaFor: Decoder: TypeGuardedDecoding,
-    T4: SchemaFor: Decoder: TypeGuardedDecoding,
-    T5: SchemaFor: Decoder: TypeGuardedDecoding,
-    T6: SchemaFor: Decoder: TypeGuardedDecoding,
-    T7: SchemaFor: Decoder: TypeGuardedDecoding
+      T1: SchemaFor: Decoder: TypeGuardedDecoding,
+      T2: SchemaFor: Decoder: TypeGuardedDecoding,
+      T3: SchemaFor: Decoder: TypeGuardedDecoding,
+      T4: SchemaFor: Decoder: TypeGuardedDecoding,
+      T5: SchemaFor: Decoder: TypeGuardedDecoding,
+      T6: SchemaFor: Decoder: TypeGuardedDecoding,
+      T7: SchemaFor: Decoder: TypeGuardedDecoding
   ]: Decoder[AvroUnion7[T1, T2, T3, T4, T5, T6, T7]] with
     def decode(schema: Schema): Any => AvroUnion7[T1, T2, T3, T4, T5, T6, T7] = {
       val schema1 = AvroSchema[T1]
@@ -452,7 +457,16 @@ object AvroUnion7 {
           val decoded: T7 = Decoder[T7].decode(schema7).apply(value)
           AvroUnion7[T1, T2, T3, T4, T5, T6, T7](decoded)
         } else {
-          throw decodingException(value, schema1, schema2, schema3, schema4, schema5, schema6, schema7)
+          throw decodingException(
+            value,
+            schema1,
+            schema2,
+            schema3,
+            schema4,
+            schema5,
+            schema6,
+            schema7
+          )
         }
       }
     }
@@ -462,14 +476,14 @@ object AvroUnion7 {
 object AvroUnion8 {
 
   given [
-    T1: SchemaFor,
-    T2: SchemaFor,
-    T3: SchemaFor,
-    T4: SchemaFor,
-    T5: SchemaFor,
-    T6: SchemaFor,
-    T7: SchemaFor,
-    T8: SchemaFor
+      T1: SchemaFor,
+      T2: SchemaFor,
+      T3: SchemaFor,
+      T4: SchemaFor,
+      T5: SchemaFor,
+      T6: SchemaFor,
+      T7: SchemaFor,
+      T8: SchemaFor
   ]: SchemaFor[AvroUnion8[T1, T2, T3, T4, T5, T6, T7, T8]] with
     def schema: Schema =
       SchemaBuilder.unionOf
@@ -491,14 +505,14 @@ object AvroUnion8 {
         .endUnion
 
   given [
-    T1: Typeable: SchemaFor: Encoder,
-    T2: Typeable: SchemaFor: Encoder,
-    T3: Typeable: SchemaFor: Encoder,
-    T4: Typeable: SchemaFor: Encoder,
-    T5: Typeable: SchemaFor: Encoder,
-    T6: Typeable: SchemaFor: Encoder,
-    T7: Typeable: SchemaFor: Encoder,
-    T8: Typeable: SchemaFor: Encoder
+      T1: Typeable: SchemaFor: Encoder,
+      T2: Typeable: SchemaFor: Encoder,
+      T3: Typeable: SchemaFor: Encoder,
+      T4: Typeable: SchemaFor: Encoder,
+      T5: Typeable: SchemaFor: Encoder,
+      T6: Typeable: SchemaFor: Encoder,
+      T7: Typeable: SchemaFor: Encoder,
+      T8: Typeable: SchemaFor: Encoder
   ]: Encoder[AvroUnion8[T1, T2, T3, T4, T5, T6, T7, T8]] with
     def encode(schema: Schema): AvroUnion8[T1, T2, T3, T4, T5, T6, T7, T8] => Any = { union =>
       union.value match {
@@ -514,14 +528,14 @@ object AvroUnion8 {
     }
 
   given [
-    T1: SchemaFor: Decoder: TypeGuardedDecoding,
-    T2: SchemaFor: Decoder: TypeGuardedDecoding,
-    T3: SchemaFor: Decoder: TypeGuardedDecoding,
-    T4: SchemaFor: Decoder: TypeGuardedDecoding,
-    T5: SchemaFor: Decoder: TypeGuardedDecoding,
-    T6: SchemaFor: Decoder: TypeGuardedDecoding,
-    T7: SchemaFor: Decoder: TypeGuardedDecoding,
-    T8: SchemaFor: Decoder: TypeGuardedDecoding
+      T1: SchemaFor: Decoder: TypeGuardedDecoding,
+      T2: SchemaFor: Decoder: TypeGuardedDecoding,
+      T3: SchemaFor: Decoder: TypeGuardedDecoding,
+      T4: SchemaFor: Decoder: TypeGuardedDecoding,
+      T5: SchemaFor: Decoder: TypeGuardedDecoding,
+      T6: SchemaFor: Decoder: TypeGuardedDecoding,
+      T7: SchemaFor: Decoder: TypeGuardedDecoding,
+      T8: SchemaFor: Decoder: TypeGuardedDecoding
   ]: Decoder[AvroUnion8[T1, T2, T3, T4, T5, T6, T7, T8]] with
     def decode(schema: Schema): Any => AvroUnion8[T1, T2, T3, T4, T5, T6, T7, T8] = {
       val schema1 = AvroSchema[T1]
@@ -559,7 +573,17 @@ object AvroUnion8 {
           val decoded: T8 = Decoder[T8].decode(schema8).apply(value)
           AvroUnion8[T1, T2, T3, T4, T5, T6, T7, T8](decoded)
         } else {
-          throw decodingException(value, schema1, schema2, schema3, schema4, schema5, schema6, schema7, schema8)
+          throw decodingException(
+            value,
+            schema1,
+            schema2,
+            schema3,
+            schema4,
+            schema5,
+            schema6,
+            schema7,
+            schema8
+          )
         }
       }
     }
