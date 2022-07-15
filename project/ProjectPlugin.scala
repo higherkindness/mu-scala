70885129bd2de48e7b790ab235deb080edc838d9
47erbot <developer@47deg.com>
2022-07-15 06:01:27 +0000

    Update cats-effect, cats-effect-testkit to 3.3.14

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -18,7 +18,7 @@ object ProjectPlugin extends AutoPlugin {
 
     lazy val V = new {
       val avro4s: String                = "4.1.0"
-      val catsEffect: String            = "3.3.13"
+      val catsEffect: String            = "3.3.14"
       val dockerItScala                 = "0.9.9"
       val dropwizard: String            = "4.2.10"
       val enumeratum: String            = "1.7.0"
