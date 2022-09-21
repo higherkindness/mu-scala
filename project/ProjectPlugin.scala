ac8a8a9a8dcf1f36cfd17c38978684924927a506
fortyseven_degrees <developer@47deg.com>
2022-09-21 06:16:36 +0000

    Update grpc-all, grpc-core, grpc-netty, ... to 1.49.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -24,7 +24,7 @@ object ProjectPlugin extends AutoPlugin {
       val enumeratum: String            = "1.7.0"
       val fs2: String                   = "3.2.14"
       val fs2Grpc: String               = "2.4.12"
-      val grpc: String                  = "1.47.0"
+      val grpc: String                  = "1.49.1"
       val kindProjector: String         = "0.13.2"
       val log4cats: String              = "2.4.0"
       val log4s: String                 = "1.10.0"
