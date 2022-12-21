744b88c87f7bba7a61ae40fc54c54601e6872408
47erbot <developer@47deg.com>
2022-12-21 06:29:39 +0100

    Update metrics-core, metrics-jmx to 4.2.14

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -20,7 +20,7 @@ object ProjectPlugin extends AutoPlugin {
       val avro4s: String                = "4.1.0"
       val catsEffect: String            = "3.4.2"
       val dockerItScala                 = "0.9.9"
-      val dropwizard: String            = "4.2.13"
+      val dropwizard: String            = "4.2.14"
       val enumeratum: String            = "1.7.2"
       val fs2: String                   = "3.4.0"
       val fs2Grpc: String               = "2.5.4"
