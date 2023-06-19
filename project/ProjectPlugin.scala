56850654ae0ff93ed8c836aec093e7561fd23c09
47erbot <developer@47deg.com>
2023-06-19 07:51:44 +0200

    Update scala-collection-compat to 2.11.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -38,7 +38,7 @@ object ProjectPlugin extends AutoPlugin {
       val pbdirect: String              = "0.7.0"
       val prometheus: String            = "0.16.0"
       val pureconfig: String            = "0.17.4"
-      val scalaCollectionCompat: String = "2.10.0"
+      val scalaCollectionCompat: String = "2.11.0"
       val scalacheckToolbox: String     = "0.7.0"
       val scalamock: String             = "5.1.0"
       val scalapb: String               = "0.11.13"
