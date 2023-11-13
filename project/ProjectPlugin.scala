a4ec087852830a71fa86686501d5e4b8e2d4a90a
47erbot <developer@47deg.com>
2023-11-13 06:50:11 +0100

    Update avro4s-core to 5.0.7

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -64,7 +64,7 @@ object ProjectPlugin extends AutoPlugin {
         "com.beachape"        %% "enumeratum"  % V.enumeratum
       ).value,
       libraryDependencies ++= scalaVersionSpecificDeps(3)(
-        "com.sksamuel.avro4s" %% "avro4s-core" % "5.0.6"
+        "com.sksamuel.avro4s" %% "avro4s-core" % "5.0.7"
       ).value,
       scalacOptions --= on(2, 13)("-Wunused:patvars").value
     )
