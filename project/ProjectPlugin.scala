8f786d84c49cfb5f2e46280aeeffab31274cade5
oss-47 <functional-ops@xebia.com>
2024-10-21 05:33:25 +0000

    Update metrics-core, metrics-jmx to 4.2.28

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -21,7 +21,7 @@ object ProjectPlugin extends AutoPlugin {
       val catsEffect: String            = "3.5.4"
       val catsRetry: String             = "3.1.3"
       val dockerItScala                 = "0.12.0"
-      val dropwizard: String            = "4.2.26"
+      val dropwizard: String            = "4.2.28"
       val enumeratum: String            = "1.7.3"
       val fs2: String                   = "3.10.2"
       val fs2Grpc: String               = "2.7.16"
