a047f7ab72ca9151d692b77f8ddf18df1c7c6bb1
oss-47 <functional-ops@xebia.com>
2025-03-31 05:24:54 +0000

    Update fs2-grpc-runtime to 2.8.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -26,7 +26,7 @@ object ProjectPlugin extends AutoPlugin {
       val dropwizard: String            = "4.2.30"
       val enumeratum: String            = "1.7.5"
       val fs2: String                   = "3.12.0"
-      val fs2Grpc: String               = "2.7.22"
+      val fs2Grpc: String               = "2.8.0"
       val grpc: String                  = "1.71.0"
       val kindProjector: String         = "0.13.3"
       val log4cats: String              = "2.7.0"
