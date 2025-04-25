fdd1afc8b0194ce484df45d505fea411fe5ad129
oss-47 <functional-ops@xebia.com>
2025-04-25 05:22:41 +0000

    Update pureconfig to 0.17.9

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -40,7 +40,7 @@ object ProjectPlugin extends AutoPlugin {
       val paradise: String              = "2.1.1"
       val pbdirect: String              = "0.7.0"
       val prometheus: String            = "0.16.0"
-      val pureconfig: String            = "0.17.8"
+      val pureconfig: String            = "0.17.9"
       val scalaCollectionCompat: String = "2.13.0"
       val scalacheckToolbox: String     = "0.7.0"
       val scalamock: String             = "5.1.0"
